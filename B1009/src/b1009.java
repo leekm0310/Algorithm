60cbdaf555a0e186326fa881b65edda45a04dab2
leekm0310 <leekm310@gmail.com>
2022-01-05 15:46:36 +0900

    백준 1009 - 분산처리 완료 1의 숫자만 있으면 된다는 생각에 a,b모두 10으로 나눴는데 틀렸다고 나와서 고생 + 배열에 넣어서 빼내었는데 틀렸다고 나와서 고생..

--- B1009/src/b1009.java
+++ B1009/src/b1009.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class b1009 {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner scn = new Scanner(System.in);
+        int testcases = scn.nextInt();
+        for (int i = 0; i < testcases; i++) {
+            //1의 숫자만 필요하다고 생각해서 a,b도 10으로 나눴는데 틀림
+            int a = scn.nextInt();
+            int b = scn.nextInt();
+            int c = 1;
+            //answer[i] = Math.pow(a,b) % 10.0; 뻘짓의 흔적
+            for (int j = 0; j < b; j++){
+                c = a * c % 10;
+            }
+            if (c == 0) {
+                c = 10;
+            }
+            System.out.println(c);
+        }
+        scn.close();
     }
 }
