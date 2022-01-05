181eaa234b508a807dbd5648c3044249aad15c29
leekm0310 <leekm310@gmail.com>
2022-01-05 20:58:20 +0900

    백준 1076 - 저항 다시 한번 천천히 보다보니.... 좀 더 코드를 깔끔하게 짤 수 있을 거 같아서 수정하고 int타입을 모두 long으로 바꿈성공...!

--- B1076/src/B1076.java
+++ B1076/src/B1076.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class B1076 {
+import java.util.Scanner;
+
+public class B1076 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String[] color = {"black", "brown", "red", "orange",
+        "yellow", "green", "blue", "violet", "grey", "white"};
+
+        String color1 = scn.nextLine();
+        String color2 = scn.nextLine();
+        String color3 = scn.nextLine();
+        long c1 = 0;
+        long c2 = 0;
+        long c3 = 0;
+        long answer = 0;
+        for (int i = 0; i < color.length; i++){
+            if (color[i].equals(color1)){
+                c1 = i * 10;
+            }
+            if (color[i].equals(color2)){
+                c2 = i;
+            }
+            if (color[i].equals(color3)){
+                c3 = (int)Math.pow(10, i);
+            }
+        }
+        scn.close();
+        answer = (c1 + c2) * c3;
+        System.out.println(answer);
+
+    }
 }
