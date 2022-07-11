af17410204a3c2d42f4aaf4533cf3f784029dbed
leekm0310 <leekm310@gmail.com>
2022-07-11 15:08:47 +0900

    백준 2839 설탕 공장
    - 순서에 주의하자..
    - 항상 확인을 좀 더 꼼꼼히하자

--- B2839/src/Main.java
+++ B2839/src/Main.java
@@ -6,14 +6,14 @@ public class Main {
         int kg = scanner.nextInt();
         scanner.close();
 
-       if (kg % 5 == 0) {
-           System.out.println(kg / 5);
-       } else if ((kg % 5) % 3 == 0) {
-           System.out.println((kg / 5) + ((kg % 5) / 3));
-       } else if ((kg % 5) % 3 != 0 && kg % 3 == 0) {
-           System.out.println(kg / 3);
-       } else if ((kg % 5) % 3 != 0) {
-           System.out.println(-1);
-       }
+        if (kg == 4 || kg == 7) {
+            System.out.println(-1);
+        } else if (kg % 5 == 0) {
+            System.out.println(kg / 5);
+        } else if (kg % 5 == 1 || kg % 5 == 3) {
+            System.out.println((kg / 5) + 1);
+        } else if (kg % 5 == 2 || kg % 5 == 4) {
+            System.out.println((kg / 5) + 2);
+        }
     }
 }
