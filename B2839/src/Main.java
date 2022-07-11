519f2fa5da9bdafb3c0f194d3d5447d57d4982bf
leekm0310 <leekm310@gmail.com>
2022-07-11 14:51:33 +0900

    백준 2839 설탕 공장
    - 2차 실패: 답은 맞는데 메모리 초과로 실패하는걸까..

--- B2839/src/Main.java
+++ B2839/src/Main.java
@@ -5,23 +5,15 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int kg = scanner.nextInt();
         scanner.close();
-        int x;
-        int y;
-        int answer = 0;
 
        if (kg % 5 == 0) {
-           x = kg / 5;
-           answer = x;
+           System.out.println(kg / 5);
        } else if ((kg % 5) % 3 == 0) {
-           x = kg / 5;
-           y = (kg % 5) / 3;
-           answer = x + y;
+           System.out.println((kg / 5) + ((kg % 5) / 3));
        } else if ((kg % 5) % 3 != 0 && kg % 3 == 0) {
-           y = kg / 3;
-           answer = y;
+           System.out.println(kg / 3);
        } else if ((kg % 5) % 3 != 0) {
-           answer = -1;
+           System.out.println(-1);
        }
-        System.out.println(answer);
     }
 }
