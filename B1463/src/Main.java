b11e640675070ecb733eb15e7e8c6cbaa927f895
leekm0310 <leekm310@gmail.com>
2022-07-12 12:00:13 +0900

    백준 1463 1로 만들기
    - dp + 재귀 사용해서 풀어야함.

--- B1463/src/Main.java
+++ B1463/src/Main.java
@@ -1,27 +1,30 @@
 import java.util.Scanner;
 
 public class Main {
+    static Integer[] dp;
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int a = scn.nextInt();
 
-        int answer = 0;
-        cal(a, answer);
-        System.out.println(answer);
+        dp = new Integer[a + 1];
+        dp[0] = dp[1] = 0;
+
+        System.out.println(cal(a));
     }
 
-    public static void cal(int a, int answer) {
-        if (a == 1) {
-            return;
+    public static int cal(int a) {
+        if (dp[a] == null) {
+            if (a % 6 == 0) {
+                dp[a] = Math.min(cal(a - 1), Math.min(cal(a / 3), cal(a / 2))) + 1;
+            } else if (a % 3 == 0) {
+                dp[a] = Math.min(cal(a / 3), cal(a - 1)) + 1;
+            } else if (a % 2 == 0) {
+                dp[a] = Math.min(cal(a / 2), cal(a - 1)) + 1;
+            } else {
+                dp[a] = cal(a - 1) + 1;
+            }
         }
-        if (a % 3 == 0) {
-            a = a / 3;
-            answer++;
-        }
-        if (a % 2 == 0) {
-            a = a / 2;
-            answer++;
-        }
-        cal(a - 1, answer+1);
+        return dp[a];
     }
 }
