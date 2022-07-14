ae6f8407a6d3a2407b187a586cf122921bad868a
leekm0310 <leekm310@gmail.com>
2022-07-14 14:18:19 +0900

    [미완] 백준 7662 이중 우선순위 큐
    - 풀이과정: 우선순위 큐를 가지고 구현을 해보려 노력했지만 역부족. 구글링 끝에 treemap이라는걸 알게됨
    - 맞왜틀..의 늪에 빠졌고 어디가 틀린건지도 모르겠어서 다른 문제 풀고 다시 봐야함.

---
+++ B7662/src/Main.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int totalCase = Integer.parseInt(br.readLine());
+
+        TreeMap<Integer, Integer> treeMap = new TreeMap();
+
+        for (int j = 0; j < totalCase; j++) {
+            int testcase = Integer.parseInt(br.readLine());
+
+            for (int i = 0; i < testcase; i++) {
+                String[] commandLine = br.readLine().split(" ");
+                char command = commandLine[0].charAt(0);
+                int number = Integer.parseInt(commandLine[1]);
+
+                if (command == 'I') {
+                    treeMap.put(number, treeMap.getOrDefault(number, 0) + 1);
+                } else if (command == 'D') {
+                    if (treeMap.isEmpty()) {
+                        continue;
+                    }
+
+                    int key = number == 1 ? treeMap.lastKey() : treeMap.firstKey();
+                    if (treeMap.put(key, treeMap.get(key) - 1) == 1) {
+                        treeMap.remove(key);
+                    }
+                }
+            }
+
+            if (treeMap.isEmpty()) {
+                System.out.println("EMPTY");
+            } else {
+                System.out.println(treeMap.lastKey() + " " + treeMap.firstKey());
+            }
+        }
+
+    }
+}
+
+
