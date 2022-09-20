376479ffd963143f85146fe27018b0fe49975143
leekm0310 <leekm310@gmail.com>
2022-09-20 16:41:16 +0900

    [완료] 인프런 알고리즘 5-2
    - peek가 어떤 값인지 확인하고 빼내는 로직 작성

---
+++ Inflearn01/src/Inflearn52.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Inflearn52 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String[] arr = scn.next().split("");
+
+        Stack<String> stack = new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i].equals(")")) {
+                while (!stack.peek().equals("(")) {
+                    stack.pop();
+                }
+                stack.pop();
+            } else {
+                stack.push(arr[i]);
+            }
+        }
+
+        String answer = "";
+        for (int i = 0; i < stack.size(); i++) {
+            answer += stack.get(i);
+        }
+        System.out.println(answer);
+    }
+}
