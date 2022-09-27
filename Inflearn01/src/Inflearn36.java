95b82afb8d45735df5197b64fdec409b6956b341
leekm0310 <leekm310@gmail.com>
2022-09-27 17:42:15 +0900

    [완료] 인프런 알고리즘 3-6
    - 투포인터로 풀어야한다

---
+++ Inflearn01/src/Inflearn36.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Inflearn36 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int k = scn.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        int answer = 0;
+        int cnt = 0;
+        int lt = 0;
+        for (int rt = 0; rt < n; rt++) {
+            if (arr[rt] == 0) {
+                cnt++;
+            }
+            while (cnt > k) {
+                if (arr[lt] == 0) {
+                    cnt--;
+                }
+                lt++;
+            }
+            answer = Math.max(answer, rt - lt + 1);
+        }
+
+        System.out.println(answer);
+    }
+}
