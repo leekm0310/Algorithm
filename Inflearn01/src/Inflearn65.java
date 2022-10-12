1c9839ae91e310fa29e4c86333ba56f168ba4b5c
leekm0310 <leekm310@gmail.com>
2022-10-12 15:03:04 +0900

    [완료] 인프런 알고리즘 6-5
    - 정렬로 푼다면 nlogn. 해쉬맵보다는 성능이 떨어지지만 이렇게도 풀 수 있다.

---
+++ Inflearn01/src/Inflearn65.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Inflearn65 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        String answer = "U";
+        Arrays.sort(arr);
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                answer = "D";
+            }
+        }
+        System.out.println(answer);
+    }
+}
