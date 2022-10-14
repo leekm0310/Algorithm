b5eced9da069a7492f4b449654e91f9bb3b9a4a9
leekm0310 <leekm310@gmail.com>
2022-10-14 15:09:21 +0900

    [완료] 인프런 알고리즘 6-9

---
+++ Inflearn01/src/Inflearn69.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Inflearn69 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        int lt = Arrays.stream(arr).max().getAsInt();
+        int rt = Arrays.stream(arr).sum();
+        int answer = 0;
+
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(arr, mid) <= m) {
+                answer = mid;
+                rt = mid - 1;
+            } else {
+                lt = mid + 1;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    public static int count(int[] arr, int capacity) {
+        int cnt = 1;
+        int sum = 0;
+        for (int x : arr) {
+            if (sum + x > capacity) {
+                cnt++;
+                sum = x;
+            } else {
+                sum += x;
+            }
+        }
+        return cnt;
+    }
+}
