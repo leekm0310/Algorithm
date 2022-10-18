39b400e8a5d43cd25a2a84979786d68723553a48
leekm0310 <leekm310@gmail.com>
2022-10-18 16:43:01 +0900

    [진행중] 인프런 알고리즘 8-1
    - 맞왜틀의 늪에 빠짐;; 다시 공부하고 풀어봐야 할 것 같다.

---
+++ Inflearn01/src/Inflearn81.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Inflearn81 {
+    static String answer = "NO";
+    static int n = 0;
+    static int total = 0;
+    static boolean flag = false;
+    public static void DFS(int L, int sum, int[] arr) {
+        if (flag) {
+            return;
+        }
+        if (sum > total / 2) {
+            return;
+        }
+        if (L == n) {
+            if ((total - sum) == sum) {
+                answer = "YES";
+                flag = true;
+            }
+        } else {
+            DFS(L + 1, sum + arr[L], arr);
+            DFS(L + 1, sum, arr);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+            total += arr[i];
+        }
+        DFS(0,0,arr);
+        System.out.println(answer);
+    }
+}
