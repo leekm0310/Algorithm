b4ff9a10b5b253d50ef52dd45f818f69ea86e6f0
leekm0310 <leekm310@gmail.com>
2023-01-10 13:55:02 +0900

    [완료] 프로그래머스 레벨1 하샤드 수

---
+++ Programmers/src/Level1_04.java
@@ -0,0 +1,19 @@
+public class Level1_04 {
+    public static boolean solution(int x) {
+        boolean answer = false;
+        int a = x;
+        int sum = 0;
+        while (a > 0) {
+            sum += a % 10;
+            a = a / 10;
+        }
+        if (x % sum == 0) {
+            answer = true;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(13));
+    }
+}
