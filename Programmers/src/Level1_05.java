ac02877a18e74ad9ea09f7e77117bdac451e752e
leekm0310 <leekm310@gmail.com>
2023-01-10 14:11:24 +0900

    [완료] 프로그래머스 레벨1 - 정수 내림차순으로 배치하기

---
+++ Programmers/src/Level1_05.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Level1_05 {
+    public static long solution(long n) {
+        long answer = 0;
+
+        String[] split = String.valueOf(n).split("");
+        Arrays.sort(split);
+        StringBuilder sb = new StringBuilder();
+        for (String s : split) {
+            sb.append(s);
+        }
+        answer = Long.parseLong(sb.reverse().toString());
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(118372));
+    }
+}
