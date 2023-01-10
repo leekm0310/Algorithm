2ccdb5a00d4a9593db47b8227610830ddb98b92d
leekm0310 <leekm310@gmail.com>
2023-01-10 14:15:25 +0900

    [완료] 프로그래머스 레벨1 - 나머지가 1이 되는 수 찾기

---
+++ Programmers/src/Level1_06.java
@@ -0,0 +1,15 @@
+public class Level1_06 {
+    public static int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 1) {
+                return i;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(12));
+    }
+}
