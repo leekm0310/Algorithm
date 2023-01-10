f4e60d5aa27cf6c03a0b6e50ace2f03fb6506168
leekm0310 <leekm310@gmail.com>
2023-01-10 14:24:09 +0900

    [완료] 프로그래머스 레벨1 - 서울에서 김서방 찾기

---
+++ Programmers/src/Level1_07.java
@@ -0,0 +1,12 @@
+public class Level1_07 {
+    public String solution(String[] seoul) {
+        int answer = 0;
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                answer = i;
+                break;
+            }
+        }
+        return "김서방은 " + answer +"에 있다";
+    }
+}
