62b8285253dbec8a98c0da1c578c4b843c3a0a76
leekm0310 <leekm310@gmail.com>
2023-01-12 16:02:16 +0900

    [완료] 프로그래머스 레벨1 - 문자열 다루기 기본

---
+++ Programmers/src/Level1_15.java
@@ -0,0 +1,19 @@
+public class Level1_15 {
+    public static boolean solution(String s) {
+        boolean answer = false;
+        if (s.length() == 4 | s.length() == 6) {
+            for (int i = 0; i < 10; i++) {
+                s = s.replaceAll(String.valueOf(i), "");
+            }
+            if (s.equals("")) {
+                answer = true;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("00000"));
+    }
+}
