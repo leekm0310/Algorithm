df9f49fe33bbb44d264b4286384e26018fbcdeed
leekm0310 <leekm310@gmail.com>
2023-01-12 17:05:58 +0900

    [완료] 프로그래머스 레벨1 - 같은 숫자는 싫
    - 스택으로 풀었는데 리스트로 푸는 방법이 효율성은 조금 더 좋았다.

---
+++ Programmers/src/Level1_18.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+
+public class Level1_18 {
+    public Stack<Integer>  solution(int []arr) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (!stack.peek().equals(arr[i])) {
+                stack.push(arr[i]);
+            }
+        }
+        return stack;
+    }
+}
