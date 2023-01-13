d542e6239aadc9038b2c67561384c9857e25b0be
leekm0310 <leekm310@gmail.com>
2023-01-13 23:37:25 +0900

    [완료] 프로그래머스 레벨1 - 3진법 뒤집기

---
+++ Programmers/src/Level1_19.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Level1_19 {
+    public static int solution(int n) {
+        int answer = 0;
+        List<Integer> list = new ArrayList<>();
+        while (n != 0) {
+            list.add(n % 3);
+            n = n / 3;
+        }
+        int a = 1;
+        for (int i = list.size() - 1; i >= 0; i--) {
+            answer += list.get(i) * a;
+            a *= 3;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(45));
+    }
+}
