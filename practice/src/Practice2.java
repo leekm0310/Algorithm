2ef737376a36a302b22c167c4e64da5ee17cffc6
leekm0310 <leekm310@gmail.com>
2023-02-07 23:51:21 +0900

    [완료] 버블정렬 기본문제 연습

---
+++ practice/src/Practice2.java
@@ -0,0 +1,19 @@
+public class Practice2 {
+    //버블정렬
+    public static int[] solution(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        int[] arr = {13,5,11,7,23,15};
+        solution(arr);
+    }
+}
