a0cb19263c6d1476aa618914e0a2b02db8eba530
leekm0310 <leekm310@gmail.com>
2023-02-21 23:13:35 +0900

    [완료] 프로그래머스 레벨 1 - 다트게임

--- Programmers/src/level1/Level1_32.java
+++ Programmers/src/level1/Level1_32.java
@@ -6,40 +6,56 @@ import java.util.List;
 public class Level1_32 {
     public static int solution(String dartResult) {
         int answer = 0;
-        List<Integer> list = new ArrayList<>();
-        String[] split = dartResult.split("");
-        char[] array = dartResult.toCharArray();
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] == 'S') {
-                list.add((int) Math.pow(Double.parseDouble(String.valueOf(array[i - 1])), 1));
-            } else if (array[i] == 'D') {
-                list.add((int) Math.pow(Double.parseDouble(String.valueOf(array[i - 1])), 2));
-            } else if (array[i] == 'T') {
-                list.add((int) Math.pow(Double.parseDouble(String.valueOf(array[i - 1])), 3));
-            } else if (array[i] == '*') {
-                int nums = i / 2;
-                if (nums == 1) {
-                    list.set(0, list.get(0) * 2);
-                } else {
-                    list.set(nums - 1, list.get(nums -1) * 2);
-                    list.set(nums - 2, list.get(nums -2) * 2);
-                }
-            } else if (array[i] == '#') {
-                int nums = i / 2;
-                list.set(nums - 1, list.get(nums -1) * -1);
-            } else if (array[i] == '0' && !Character.isLetter(array[i-1])) {
-//                array[i] = '10'; 라고 쓰고 싶었는데 생각해보니 char 형이라 안됨.......ㅠㅠ
+        String[] split = dartResult.split("[A-Z]|\\*|#");
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < split.length; i++) {
+            if (!split[i].equals("")) {
+                nums.add(Integer.parseInt(split[i]));
             }
         }
 
-        for (Integer x : list) {
-            answer += x;
+        List<String> options = new ArrayList<>();
+        String s = dartResult.replaceAll("[0-9]", "");
+        String[] split1 = s.split("");
+        for (int i = 0; i < split1.length; i++) {
+            if (!split1[i].equals("")) {
+                options.add(split1[i]);
+            }
         }
+
+        int round = 0;
+
+        for (int i = 0; i < options.size(); i++) {
+            if (options.get(i).equals("D")) {
+                nums.set(round, (int) Math.pow(nums.get(round), 2));
+                round++;
+            } else if (options.get(i).equals("T")) {
+                nums.set(round, (int) Math.pow(nums.get(round), 3));
+                round++;
+            } else if (options.get(i).equals("S")) {
+                round++;
+            } else if (options.get(i).equals("*")) {
+                if (i == 1) {
+                    nums.set(0, nums.get(0) * 2);
+                } else {
+                    nums.set(round - 1, nums.get(round - 1) * 2);
+                    nums.set(round - 2, nums.get(round - 2) * 2);
+                }
+            } else if (options.get(i).equals("#")) {
+                nums.set(round - 1, nums.get(round - 1) * -1);
+            }
+        }
+
+        for (Integer num : nums) {
+            answer += num;
+        }
+
         return answer;
     }
 
     public static void main(String[] args) {
-        String s = "1D2S#10S";
+        String s = "1T2D3D#";
+        String a = "1D2S0T";
         System.out.println(solution(s));
 
     }
