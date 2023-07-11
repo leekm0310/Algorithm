3f8c4b1b94a09679d4f8f4317f7be24171d9264e
leekm0310 <leekm310@gmail.com>
2023-07-11 15:56:53 +0900

    [완료] Leetcode - 205. Isomorphic Strings

---
+++ Leetcode/src/Leetcode205.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+
+public class Leetcode205 {
+
+    public static boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Character> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                if (!map.get(s.charAt(i)).equals(t.charAt(i))) {
+                    return false;
+                }
+            } else {
+                if (map.containsValue(t.charAt(i))) {
+                    return false;
+                }
+                map.put(s.charAt(i), t.charAt(i));
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "title";
+        String t = "paper";
+        System.out.println(isIsomorphic(s, t));
+    }
+}
