360eb4cd7bbb4e831daf3bc4e4d9fcbc59ee1eb4
leekm0310 <leekm310@gmail.com>
2023-08-04 15:41:04 +0900

    [완료] 백준 15649 - N과 M (11)

---
+++ Baekjoon/src/B15665.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B15665 {
+    public static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+
+        int[] arr = new int[n];
+        int[] result = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        Arrays.sort(arr);
+        dfs(n, m, 0, arr, result);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int n, int m, int depth, int[] arr, int[] result) {
+        if (depth == m) {
+            for (int i : result) {
+                sb.append(i + " ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        int before = 0;
+        for (int i = 0; i < n; i++) {
+            if (before != arr[i]) {
+                result[depth] = arr[i];
+                before = arr[i];
+                dfs(n, m, depth + 1, arr, result);
+            }
+        }
+    }
+}
